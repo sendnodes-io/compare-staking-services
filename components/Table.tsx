4a9229f47a27529fa412a642ea339efac4874d28
BigBoss <bigboss@sendnodes.io>
2022-12-13 21:01:49 -0600

    add formulas

--- components/Table.tsx
+++ components/Table.tsx
@@ -278,6 +278,14 @@ export default function Table() {
                               Rewards from validators are not included in this
                               calculation.{" "}
                             </li>
+                            <li>
+                              15k Node Net = [(Daily Gross per 15k - (daily fix
+                              fee / 7 day avg pokt price)] * Rev share
+                            </li>
+                            <li>
+                              60k Node Net = [(Daily Gross per 15k (4) - (daily
+                              fix fee / 7 day avg pokt price)] * (4)Rev share
+                            </li>
                           </ul>
                         </div>
                       </div>
